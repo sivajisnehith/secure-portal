740c38db27d34ff2c6419ea863d509d5ed74e77b
chandolu sivaji snehith <snehithsivaji@gmail.com>
2026-02-01 01:50:19 -0500

    Add files via upload

---
+++ sendmail.py
@@ -0,0 +1,14 @@
+import smtplib   #This is used to connect to mail server and send mails
+s = smtplib.SMTP('smtp.gmail.com',587)
+s.ehlo()
+s.starttls()
+s.login("snehith.24bcs7081@vitapstudent.ac.in","ajzg emkp wkap cfvu")
+message = """Subject: Test Mail
+He is too hot
+"""
+s.sendmail("snehith.24bcs7081@vitapstudent.ac.in",
+           "snehithsivaji@gmail.com",
+           message
+)
+s.quit()
+
